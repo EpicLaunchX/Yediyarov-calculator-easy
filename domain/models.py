54baed3c4e94796321e237a7a1d2acebd668c02f
Yediyarov <yediyarov.xeyal@gmail.com>
2024-04-18 11:07:00 +0400

    fix: solve task1

---
+++ domain/models.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class Operands:
+    first_operand: int
+    second_operand: int
+
+    def __post_init__(self):
+        if not isinstance(self.first_operand, int) or not isinstance(self.second_operand, int):
+            raise TypeError("Both operands must be integers")
