501eacfc72abb702cba9453ca41f8278dd871cb8
Yediyarov <yediyarov.xeyal@gmail.com>
2024-04-19 15:05:31 +0400

    feat: add new method

--- src/pytemplate/domain/models.py
+++ src/pytemplate/domain/models.py
@@ -9,3 +9,10 @@ class Operands:
     def __post_init__(self):
         if not isinstance(self.first_operand, int) or not isinstance(self.second_operand, int):
             raise TypeError("Both operands must be integers")
+
+
+# Define this function outside of the class
+def operands_factory(first_operand: int, second_operand: int) -> Operands:
+    if not isinstance(first_operand, int) or not isinstance(second_operand, int):
+        raise TypeError("Both operands must be integers.")
+    return Operands(first_operand, second_operand)
